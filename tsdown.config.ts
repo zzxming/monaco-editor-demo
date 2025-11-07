bba234932264874d1b341c910b5c6010032c7c15
zzxming <zzxming77@163.com>
2025-11-07 21:51:26 +0800

    ci: deploy

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,3 +1,5 @@
+import { copyFile } from 'node:fs/promises';
+import { globSync } from 'glob';
 import { defineConfig } from 'tsdown';
 
 export default defineConfig([
@@ -10,6 +12,15 @@ export default defineConfig([
       name: 'bundle',
       format: 'iife',
       exports: 'named',
+    },
+    onSuccess() {
+      const files = globSync('dist/*.iife.js');
+      return Promise.all(
+        files.map((file) => {
+          const fileName = file.split(/[/\\]/).pop();
+          return copyFile(file, `docs/${fileName}`);
+        }),
+      ) as unknown as Promise<void>;
     },
   },
   {
